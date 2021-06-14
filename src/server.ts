9813aa9e5ae8ab18fe22769583f266aa34d3c902
Danilo LM <danilolopesdemoraes@gmail.com>
2021-06-14 11:47:48 -0300

    feat: docker with postgres

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+import "./database";
 
 const app = express();
 
@@ -9,4 +10,4 @@ app.use(express.json());
 app.use(router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(3333, () => console.log("Server is runnig..."));
+app.listen(3333, () => console.log("Server is runnig......"));
